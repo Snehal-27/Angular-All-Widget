b11044f6cd96a85b7b0ce44b4e8d11c256fbeac9
Snehal Mundada <Snehal.Mundada@internal.advancedriskanalytics.com>
2020-12-20 18:46:04 +0530

    updating to latest angular

--- src/app/pivot/pivot/pivot.component.spec.ts
+++ src/app/pivot/pivot/pivot.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PivotComponent } from './pivot.component';
 
@@ -6,7 +6,7 @@ describe('PivotComponent', () => {
   let component: PivotComponent;
   let fixture: ComponentFixture<PivotComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PivotComponent ]
     })
